7467bc0a77b5c53126ef6c38bbc2a80a2906e639
Jaan Kalbin <jaan.kalbin@tktk.ee>
2023-01-22 23:23:08 +0200

    add Week 8 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter something:");
+string userInput = Console.ReadLine();
+PrintAnyWord(userInput); //userInput - argument
+static void PrintAnyWord(string anyString) //anyString - parameter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}static void PrintHello() //function ehk meetod
+{
+    Console.WriteLine("Hello, world!");
+}
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
